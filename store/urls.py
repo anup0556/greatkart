4c3896da205dafb101e50563622a8ca66d2ad388
ANUP KUMAR <anup.0556@gmail.com>
2024-03-07 08:54:54 +0530

    category added

--- store/urls.py
+++ store/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .import views
+from .import views  # Corrected import statement
 
 urlpatterns = [
-    path('', views.store,name='store' ),
-] 
+    path('', views.store, name='store'),
+    path('<slug:category_slug>/', views.store, name='products_by_category'),
+]
+
--- store/views.py
+++ store/views.py
@@ -1,9 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import product
+from category.models import category
 
-def store(request):
-    products = product.objects.all().filter(is_available=True)
-    product_count = products.count()
+def store(request,category_slug=None):
+    categorys = None
+    products = None
+    
+    if category_slug != None:
+        categorys = get_object_or_404(category,slug=category_slug)
+        products = product.objects.filter(category=categorys,is_available=True)
+        product_count = products.count()
+    else:   
+        products = product.objects.all().filter(is_available=True)
+        product_count = products.count()
     context = {
         'products': products,
         'product_count ': product_count,
